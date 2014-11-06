4ce07652534908513ca2d68c9634a994a8e5ee84
ysusuk <yurasusuk@gmail.com>
2014-11-06 12:04:46 +0100

    findAndFilterCandidates

--- src/main/scala/Apriori.scala
+++ src/main/scala/Apriori.scala
@@ -1,17 +1,42 @@
 /**
  * Created by iurii.susuk on 05.11.2014.
  */
-trait Item
 
-class I1(id: String = "item 1") extends Item
-class I2(id: String = "item 2") extends Item
-class I3(id: String = "item 3") extends Item
-class I4(id: String = "item 4") extends Item
-class I5(id: String = "item 5") extends Item
+abstract class UniqueItem(val id: String) {
 
-class Apriori(transactions: Set[Set[Item]]) {
+  def canEqual(other: Any): Boolean = other.isInstanceOf[UniqueItem]
 
-  def frequency(): Map[Item, Integer] = {
-     null
+  override def equals(other: Any): Boolean = other match {
+    case that: UniqueItem =>
+      (that canEqual this) &&
+        id == that.id
+    case _ => false
+  }
+
+  override def hashCode(): Int = {
+    val state = Seq(id)
+    state.map(_.hashCode()).foldLeft(0)((a, b) => 31 * a + b)
+  }
+
+  override def toString = {
+    id
+  }
+
+}
+
+class I1(override val id: String = "item 1") extends UniqueItem(id)
+
+class I2(override val id: String = "item 2") extends UniqueItem(id)
+
+class I3(override val id: String = "item 3") extends UniqueItem(id)
+
+class I4(override val id: String = "item 4") extends UniqueItem(id)
+
+class I5(override val id: String = "item 5") extends UniqueItem(id)
+
+class Apriori(val transactions: List[List[UniqueItem]], val minSupportCount: Int = 2) {
+
+  def findAndFilterCandidates(): Map[UniqueItem, Int] = {
+    transactions.flatten groupBy (i => i) mapValues (_.size) filter (_._2 >= minSupportCount)
   }
 }
--- src/test/scala/AprioriSpec.scala
+++ src/test/scala/AprioriSpec.scala
@@ -4,11 +4,22 @@ import org.scalatest.{Matchers, FlatSpec}
  * Created by iurii.susuk on 05.11.2014.
  */
 class AprioriSpec extends FlatSpec with Matchers {
-  val transactions = Set(Set(new I1, new I2))
+  val transactions = List(
+    List(new I1, new I2, new I5),
+    List(new I2, new I4),
+    List(new I2, new I3),
+    List(new I1, new I2, new I4),
+    List(new I1, new I3),
+    List(new I2, new I3),
+    List(new I1, new I3),
+    List(new I1, new I2, new I3, new I5),
+    List(new I1, new I2, new I3))
+
+  val candidates: Map[UniqueItem, Integer] = Map(new I1 -> 6)
 
   "Apriori" should "" in {
     val apriori = new Apriori(transactions)
-    val candidates = apriori.frequency()
-
+    val candidates = apriori.findAndFilterCandidates()
+    println(candidates)
   }
 }
